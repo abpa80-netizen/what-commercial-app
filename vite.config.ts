0319f48614dfcd1597eaa1bf37e3a7ef59bf00e0
abpa80-netizen <ab.pa80@gmail.com>
2026-09-24 22:00:46 +0100

    Add Vite configuration for React application

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/what-commercial-app/',
+  build: {
+    outDir: "dist",
+  },
+});
